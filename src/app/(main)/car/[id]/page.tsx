d26288f32ec309ba86a3ad53aaa5a5594ec20b7d
Jordan Valdez <jvaldezq1101@gmail.com>
2025-05-14 21:45:56 -0600

    Adding more metadata to the car page

--- src/app/(main)/car/[id]/page.tsx
+++ src/app/(main)/car/[id]/page.tsx
@@ -11,14 +11,53 @@ type Props = {
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const id = (await params).id;
-
   const data = await fetchCarById(id);
 
-  const { year, make, model, trim, transType, condition } = data;
-  const title = `${year} ${make} ${model} ${trim} - Transmisión ${transType}, Condición ${condition}`;
-  const description = `Descubre este impecable`;
+  const {
+    year,
+    make,
+    model,
+    trim,
+    transType,
+    condition,
+    thumbnail,
+    city,
+    state,
+    price,
+    mileage,
+    mileageType,
+  } = data;
 
-  return { title, description };
+  const title = `${year} ${make} ${model} ${trim} en ${state}`;
+  const description = `Compra este ${year} ${make} ${model} ${trim}, ${condition}, ${transType}, ${mileage} ${mileageType}, por $${price.toLocaleString()} en ${state}. ¡Haz clic para más detalles!`;
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      type: 'article',
+      url: `https://carrocr.com/car/${id}`,
+      images: [
+        {
+          url: thumbnail,
+          width: 1200,
+          height: 630,
+          alt: `${year} ${make} ${model} ${trim}`,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+      images: [thumbnail],
+    },
+    alternates: {
+      canonical: `https://carrocr.com/car/${id}`,
+    },
+  };
 }
 
 export default async function Car({ params }: Props) {
