c4ff094139bd6b99eed68a5affe7c08e6c4bc264
Jordan Valdez <jvaldezq1101@gmail.com>
2025-07-04 18:12:24 -0600

    Handle iphone front camera space

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -6,7 +6,7 @@ import Logo from '@/assets/logo.webp';
 
 export const Header = () => {
   return (
-    <header className="flex w-full p2 items-center justify-center bg-white bg-gradient-to-t from-0% to-25% from-white to-transparent">
+    <header className="flex w-full pt-[env(safe-area-inset-top)] px-4 pb-2 items-center justify-center bg-white bg-gradient-to-t from-0% to-25% from-white to-transparent">
       <Link href="/">
         <Image
           src={Logo}
